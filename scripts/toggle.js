2e598ecbe1fbc862d1742cc0b112eb15a495ac06
smabari <mdazizulbarishafin@gmail.com>
2025-03-01 23:37:07 +0600

    optimized toggle.js

--- scripts/toggle.js
+++ scripts/toggle.js
@@ -1,9 +1,17 @@
-document.getElementById("cash-out-section").style.display = "none";
+// document.getElementById("cash-out-section").style.display = "none";
+handleToggle("cash-out-section", "none");
 document.getElementById("add-money-box").addEventListener("click", function () {
-  document.getElementById("cash-out-section").style.display = "none";
-  document.getElementById("add-money-section").style.display = "block";
+  handleToggle("cash-out-section", "none");
+  handleToggle("add-money-section", "block");
+  // document.getElementById("cash-out-section").style.display = "none";
+  // document.getElementById("add-money-section").style.display = "block";
 });
 document.getElementById("cash-out-box").addEventListener("click", function () {
-  document.getElementById("cash-out-section").style.display = "block";
-  document.getElementById("add-money-section").style.display = "none";
+  handleToggle("cash-out-section", "block");
+  handleToggle("add-money-section", "none");
+  // document.getElementById("cash-out-section").style.display = "block";
+  // document.getElementById("add-money-section").style.display = "none";
 });
+function handleToggle(id, status) {
+  document.getElementById(id).style.display = status;
+}

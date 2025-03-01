222cd2d5c4bbc4d0d07cd0d5a9bd5cab584242cd
smabari <mdazizulbarishafin@gmail.com>
2025-03-02 00:04:35 +0600

    optimized cashout.js

--- scripts/cashout.js
+++ scripts/cashout.js
@@ -3,18 +3,24 @@ document
   .addEventListener("click", function (event) {
     event.preventDefault();
     // console.log("cash")
-    const accountNumber = document.getElementById("cashout-account-number").value;
-    const amount = document.getElementById("cashout-amount").value;
-    const convertedAmount = Number(amount);
-    const pin = document.getElementById("cashout-pin").value;
-    const convertedPin = Number(pin);
-    const mainBalance = document.getElementById("main-balance").innerText;
-    const convertedMainBalance = Number(mainBalance);
+    const accountNumber = document.getElementById(
+      "cashout-account-number"
+    ).value;
+    const amount = getInputValueByID("cashout-amount");
+    // const amount = document.getElementById("cashout-amount").value;
+    // const convertedAmount = Number(amount);
+    const pin = getInputValueByID("cashout-pin");
+    // const pin = document.getElementById("cashout-pin").value;
+    // const convertedPin = Number(pin);
+    const mainBalance = getInnerTextByID("main-balance");
+    // const mainBalance = document.getElementById("main-balance").innerText;
+    // const convertedMainBalance = Number(mainBalance);
     if (accountNumber.length === 11) {
-      if (convertedPin === 1234) {
-        const newMainBalance = convertedMainBalance - convertedAmount;
-        document.getElementById("main-balance").innerText = newMainBalance;
-        // console.log(mainBalance, newMainBalance)
+      if (pin === 1234) {
+        const newMainBalance = mainBalance - amount;
+        setInnerTextByIDandValue("main-balance", newMainBalance);
+        console.log(newMainBalance, mainBalance, amount);
+        // document.getElementById("main-balance").innerText = newMainBalance;
       } else {
         alert("Enter correct Pin");
       }
--- scripts/main.js
+++ scripts/main.js
@@ -4,21 +4,19 @@ document
     event.preventDefault();
     // const amount = document.getElementById("amount").value;
     // const convertedAmount = Number(amount);
-    const amount = getInputValueByID("amount")
+    const amount = getInputValueByID("amount");
     // const pin = document.getElementById("pin").value;
     // const convertedPin = Number(pin);
-    const pin = getInputValueByID("pin")
+    const pin = getInputValueByID("pin");
     const accountNumber = document.getElementById("account-number").value;
     // const accountNumber = getInputValueByID("account-number") here we can't convert it to a number, because number doesn't have .length property
     // const mainBalance = document.getElementById("main-balance").innerText;
-    const mainBalance = getInnerTextByID("main-balance")
+    const mainBalance = getInnerTextByID("main-balance");
     // const convertedMainBalance = Number(mainBalance);
-    console.log(amount, pin, accountNumber, accountNumber.length)
     if (accountNumber.length === 11) {
-      console.log(accountNumber.length)
       if (pin === 1234) {
         const newMainBalance = mainBalance + amount;
-        setInnerTextByIDandValue("main-balance", newMainBalance)
+        setInnerTextByIDandValue("main-balance", newMainBalance);
         // document.getElementById("main-balance").innerText = newMainBalance;
       } else {
         alert("Enter correct Pin");

b283da6ad6e51cf7f1dfc305b17f36fa492d920a
smabari <mdazizulbarishafin@gmail.com>
2025-03-01 14:58:07 +0600

    login button connection

---
+++ scripts/login.js
@@ -0,0 +1,8 @@
+// alert()
+// step 1, getting the login button
+document
+  .getElementById("login-btn")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+    console.log("yo")
+  });
